cd73cb5b1912048bf4ac1f395578ed3bac5e048c
Priyanshu Samle <147939244+Priyanshu-Samle@users.noreply.github.com>
2024-07-13 11:50:06 +0530

    Update jwt.js

--- jwt.js
+++ jwt.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
- export const webToken = (req,res,next)=>{
+ export const isAuthoriztion = (req,res,next)=>{
 
     const token = req.headers.authorization.split(' ')[1];
      console.log(token);
